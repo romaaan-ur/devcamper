3a145cf70f86421927435221fe1e7084e203bfaf
Roman Ursulov <r.ursulov@akyllytilsimat.com>
2020-04-26 13:44:43 +0500

    add get, post, put, delete routers from bootcamps

--- server.js
+++ server.js
@@ -6,6 +6,34 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+  //res.status(400).json({ success: false });
+  //res.sendStatus(404);
+  //res.send({ name: 'Brad' });
+  //res.send('Hello from express');
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ succes: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ succes: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ succes: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
