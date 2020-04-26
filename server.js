361450a32d5038083196b8988a518f1ced889668
Roman Ursulov <r.ursulov@akyllytilsimat.com>
2020-04-26 22:16:28 +0500

    add colors in log message, data, first model Bootcamp

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -28,13 +29,14 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `App listening on port ${process.env.NODE_ENV} mode on port ${PORT}`
+    `App listening on port ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .bold
   )
 );
 
 // Handle unhandled rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}.red`);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
