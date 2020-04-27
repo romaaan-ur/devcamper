47495381fe6049f0eaa011e15553f91b88588617
Roman Ursulov <r.ursulov@akyllytilsimat.com>
2020-04-27 23:02:06 +0500

    add custom error handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,7 +28,8 @@ exports.getBootcamp = async (req, res, next) => {
 
     res.status(200).json({ succes: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ succes: false });
+    //res.status(400).json({ succes: false });
+    next(err);
   }
 };
 
