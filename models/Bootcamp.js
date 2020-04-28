57f3ddf0d33b2a45af7c7cd9e46fc40832704737
Roman Ursulov <r.ursulov@akyllytilsimat.com>
2020-04-28 23:30:04 +0500

    add GeoJSON Location & Geocoder Hook - MapQuest API

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -36,17 +38,15 @@ const BootcampSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please add an address'],
   },
-  /*
+
   location: {
     // GeoJson point
     type: {
       type: String,
       enum: ['Point'],
-      require: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: '2dsphere',
     },
     formattedAddress: String,
@@ -56,7 +56,7 @@ const BootcampSchema = new mongoose.Schema({
     zipcode: String,
     country: String,
   },
-  */
+
   careers: {
     //Array of strings
     type: [String],
@@ -109,4 +109,22 @@ BootcampSchema.pre('save', function (next) {
   next();
 });
 
+// Geocode and create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
+});
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
