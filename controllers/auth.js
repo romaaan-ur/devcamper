f1cee02980fe475cbb2e7995c0ba8ab619214602
Roman Ursulov <r.ursulov@akyllytilsimat.com>
2020-05-01 12:55:19 +0500

    add sign & get json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,7 @@ const User = require('../models/User');
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
+  // Create user
   const user = await User.create({
     name,
     email,
@@ -15,5 +16,8 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
